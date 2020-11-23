85966ea21f1dd7eb712a74c0005eeb75713ec3c4
harsharay <harsharay.ray@gmail.com>
2020-11-24 03:28:17 +0530

    deploying to heroku

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-export const GET_QUESTIONS = 'http://localhost:4500/api/getQuestions'
+export const GET_QUESTIONS = 'https://secure-bastion-48741.herokuapp.com/api/getQuestions'
 
-export const SAVE_FORM_DATA = 'http://localhost:4500/api/saveFormData'
+export const SAVE_FORM_DATA = 'https://secure-bastion-48741.herokuapp.com/api/saveFormData'
 
-export const GET_USER_DATA = 'http://localhost:4500/api/getUserData?username=_USERNAME_'
+export const GET_USER_DATA = 'https://secure-bastion-48741.herokuapp.com/api/getUserData?username=_USERNAME_'
 
-export const GET_USERNAME_VALIDATION = 'http://localhost:4500/api/usernameValidation?username=_USERNAME_'
+export const GET_USERNAME_VALIDATION = 'https://secure-bastion-48741.herokuapp.com/api/usernameValidation?username=_USERNAME_'
